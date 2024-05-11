82a4bae0f3f0c54355ad7160ee92b636087499d1
LofiLeon <grimmeisen.leon@gmail.com>
2024-05-12 00:57:19 +0200

    feat: add project pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
 window.onload = function() {
+    // Check for URLs ending with /index.html
     if (window.location.pathname.endsWith('/index.html')) {
       window.history.replaceState({}, document.title, window.location.pathname.slice(0, -11));
-  
+    }
+  };
