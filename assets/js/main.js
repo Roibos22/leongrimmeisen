c19edd4db17972460cb9d87c04964fcf7673b211
LofiLeon <grimmeisen.leon@gmail.com>
2024-05-11 23:20:34 +0200

    feat: create live branch

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,8 @@
 window.onload = function() {
     if (window.location.pathname.endsWith('/index.html')) {
       window.history.replaceState({}, document.title, window.location.pathname.slice(0, -11));
-  
+    }
+    if (window.location.pathname.endsWith('/')) {
+        window.history.replaceState({}, document.title, window.location.pathname.slice(0, -1));
+      }
+  };
