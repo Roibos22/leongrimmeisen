38f6f07153eb56e215e1e87a5ff32b220249b4c6
LofiLeon <grimmeisen.leon@gmail.com>
2024-05-11 15:17:47 +0200

    feat: add HealthPulse subdirectory with simple redirections

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+window.onload = function() {
+    if (window.location.pathname.endsWith('/index.html')) {
+      window.history.replaceState({}, document.title, window.location.pathname.slice(0, -11));
+  
