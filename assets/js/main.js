a1a16cb119f04f0d19bdb55ae024ff571291e22a
LofiLeon <grimmeisen.leon@gmail.com>
2024-05-20 09:51:45 +0200

    fix: mobile wieder and navbar disappear on mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,17 +7,19 @@ window.onload = function() {
 window.onload = function () {
 	const menu_btn = document.querySelector('.hamburger')
 	const mobile_menu = document.querySelector('.mobile-nav')
-	const section_btn = document.querySelector('.section-button')
+	const section_btn = document.querySelectorAll('.section-button')
 
 	menu_btn.addEventListener('click', function () {
 		menu_btn.classList.toggle('is-active');
 		mobile_menu.classList.toggle('is-active');
 	})
 
-	section_btn.addEventListener('click', function () {
-		menu_btn.classList.toggle('is-active');
-		mobile_menu.classList.toggle('is-active');
-	})
+	section_btn.forEach(button => {
+		button.addEventListener('click', function () {
+			menu_btn.classList.toggle('is-active');
+			mobile_menu.classList.toggle('is-active');
+		});
+	});
 }
 
 // RANDOM FACT GENERATOR
